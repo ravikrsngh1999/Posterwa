aa09dd8b5fe46a2ef18d04988dbf3a1dde77e19c
ravikrsngh1999 <64130660+ravikrsngh1999@users.noreply.github.com>
2021-11-29 21:30:43 +0530

    second commit

--- posterwa/settings.py
+++ posterwa/settings.py
@@ -82,13 +82,29 @@ EMAIL_USE_SSL = False
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'posterwa',
+        'USER': 'postgres',
+        'PASSWORD': 'posterwa',
+        'HOST': 'posterwa.cibyld0ek1j2.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
